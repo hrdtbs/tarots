0c7a237d90f3b650e6e2ee5721e370d525ad9a0f
hrdtbs <harada.tsubasa.0422@gmail.com>
2022-04-30 05:27:34 +0900

    chore: first commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import pluginLinaria from "@linaria/rollup";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), pluginLinaria()],
+});
